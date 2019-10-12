09ab5b8720657574d75064219b6c3b2b5feab26f
tolumide <tolumideshopein@gmail.com>
2019-10-12 20:57:58 +0100

    new config

--- webpack-build-utils/webpack.production.js
+++ webpack-build-utils/webpack.production.js
@@ -1,5 +1,3 @@
-const TerserJSPlugin = require('terser-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -16,27 +14,8 @@ module.exports = {
     ]
   },
   plugins: [
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css'
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
     })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        use: [
-          { loader: MiniCssExtractPlugin.loader },
-          'css-loader',
-          'postcss-loader'
-        ]
-      },
-      {
-        test: /\.(css|scss|sass)/i,
-        exclude: /node_modules/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-      }
-    ]
-  }
+  ]
 };
